d89c09ccf902c7af7fe1452acc84008705ffb96b
KidChoque <lucas.machado15@aluno.senai.br>
2024-05-15 08:30:15 -0300

    Finalizado Select de Cidade

--- src/components/ScheduleModal/ScheduleModal.js
+++ src/components/ScheduleModal/ScheduleModal.js
@@ -1,184 +1,196 @@
-import { useNavigation } from "@react-navigation/native"
-import { BoxModalSchedule, PacientModal, ScheduleModalView } from "../CancelAppointmentModal/Style"
-import { LinkCancel } from "../Links/style"
-import { ModalMedicalRecord } from "../MedicalRecordModal/Style"
-import { ButtonCancel, ButtonLogin, ButtonLoginVE, ButtonSchedule } from "../button/style"
-import { Input, InputScheduleModal_1, InputScheduleModal_2, InputScheduleModal_3 } from "../input/styled"
-import { ButtonTitle, ScheduleModalText, TitleModal } from "../title/style"
-import { BoxContent, BoxInput, ContainerBoxAlign } from "./Style"
-import { useEffect, useState } from "react"
-import { Text,StyleSheet } from "react-native"
-import styled from "styled-components"
-import api from "../../service/service"
-import { SelectList } from "react-native-dropdown-select-list"
+import { useNavigation } from "@react-navigation/native";
+import {
+  BoxModalSchedule,
+  PacientModal,
+  ScheduleModalView
+} from "../CancelAppointmentModal/Style";
+import { LinkCancel } from "../Links/style";
+import { ModalMedicalRecord } from "../MedicalRecordModal/Style";
+import {
+  ButtonCancel,
+  ButtonLogin,
+  ButtonLoginVE,
+  ButtonSchedule
+} from "../button/style";
+import {
+  Input,
+  InputScheduleModal_1,
+  InputScheduleModal_2,
+  InputScheduleModal_3
+} from "../input/styled";
+import { ButtonTitle, ScheduleModalText, TitleModal } from "../title/style";
+import { BoxContent, BoxInput, ContainerBoxAlign } from "./Style";
+import { useEffect, useState } from "react";
+import { Text, StyleSheet } from "react-native";
+import styled from "styled-components";
+import api from "../../service/service";
+import { SelectList } from "react-native-dropdown-select-list";
 
-
-
-const niveis = [{ id: 'D083C4DC-66E3-4856-BBFB-159764AA958D', tipo: 'Rotina' },
-{ id: '07033F9A-4E0E-4F1B-928F-08990DF17761', tipo: 'Exame' },
-{ id: '63261F6A-F19C-4651-A4DB-3601D68677E7', tipo: 'Urgencia' }]
+const niveis = [
+  { id: "D083C4DC-66E3-4856-BBFB-159764AA958D", tipo: "Rotina" },
+  { id: "07033F9A-4E0E-4F1B-928F-08990DF17761", tipo: "Exame" },
+  { id: "63261F6A-F19C-4651-A4DB-3601D68677E7", tipo: "Urgencia" }
+];
 
 export const ScheduleModal = ({
-    visible, setShowModalSchedule, route, ...rest
+  visible,
+  setShowModalSchedule,
+  route,
+  ...rest
 }) => {
-    const cidadeClinica = useState([])
-    const [agendamento, setAgendamento] = useState(null);
-    const [erroNivel, setErroNivel] = useState("");
-    const [listaLocalizacao, setListaLocalizacao] = useState([])
-    const [localizacao, setLocalizacao] = useState(null)
-    const [erroLocalizacao, setErroLocalizacao] = useState("");
+  const [erroNivel, setErroNivel] = useState("");
+  const [listaLocalizacao, setListaLocalizacao] = useState([]);
+  const [selected, setSelected] = useState("");
+  const [localizacao, setLocalizacao] = useState(null);
+  const [erroLocalizacao, setErroLocalizacao] = useState("");
+  const [agendamento, setAgendamento] = useState({ prioridadeId: null, localizacao: null });
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    async function GetCitys(){
-       await api.get("/Clinica/ListarTodas")
-       .then(response => {
-           setLocalizacao(response.data)
-           console.log("CIDADES--------------->");
-           console.log(response.data);
-       }).catch(error => {
+  async function GetCitys() {
+    await api
+      .get("/Clinica/ListarTodas")
+      .then((response) => {
+        setLocalizacao(response.data);
+      })
+      .catch((error) => {
         console.log(error);
-       })
+      });
+  }
+
+  function SetCitys() {
+    if (localizacao && localizacao.length > 0) {
+      const cidades = localizacao.map((item) => item.endereco.cidade);
+      setListaLocalizacao(cidades);
     }
+  }
 
-    function SetCitys(){
-        if (localizacao && localizacao.length > 0) {
-            const cidades = localizacao.map(item => item.endereco.cidade);
-            setListaLocalizacao(cidades);
-        }
+  function dePara(retornoApi) {
+    if (listaLocalizacao != null) {
+      let arrayOptions = [];
+      retornoApi.forEach((e) => {
+        arrayOptions.push({ value: e });
+      });
+
+      return arrayOptions;
     }
+  }
 
-
-   useEffect(() => {
+  useEffect(() => {
     GetCitys();
-}, []);
+  }, []);
 
-useEffect(() => {
+  useEffect(() => {
     SetCitys();
-    console.log(listaLocalizacao);
-}, [localizacao]);
+  }, [localizacao]);
 
+  function HandleContinue() {
+    // Verifica se o nível e a localização foram selecionados
+    if (!agendamento || agendamento.prioridadeId == null || agendamento.localizacao == null) {
+      let error = false;
+      setErroNivel("Selecione prioridade");
+      setErroLocalizacao("Preencha localização");
+      error = true;
 
-    function HandleContinue() {
-        // Verifica se o nível e a localização foram selecionados
-        if (!agendamento || agendamento.prioridadeId == null || agendamento.localizacao == null) {
-
-            let error = false
-            setErroNivel("Selecione prioridade")
-            setErroLocalizacao("Preencha localização")
-            error = true
-
-            return !error; // Se não estiverem selecionados, retorna sem fazer nada
-        }
-
-        // Esconde o modal de agendamento
-        setShowModalSchedule(false);
-
-        // Navega para a tela de seleção de clínica, passando os detalhes do agendamento
-        navigation.replace('SelectClinic', { agendamento: agendamento });
+      return !error; // Se não estiverem selecionados, retorna sem fazer nada
     }
 
+    // Esconde o modal de agendamento
+    setShowModalSchedule(false);
 
-    function Cancelar() {
+    // Navega para a tela de seleção de clínica, passando os detalhes do agendamento
+    navigation.replace("SelectClinic", { agendamento: agendamento });
+  }
 
-        setShowModalSchedule(false)
+  function Cancelar() {
+    setShowModalSchedule(false);
+  }
 
-    }
+  return (
+    <ModalMedicalRecord {...rest} visible={visible} transparent={true} animationType="fade">
+      <ScheduleModalView>
+        <BoxModalSchedule>
+          <ContainerBoxAlign>
+            <TitleModal>Agendar consulta</TitleModal>
+            <BoxContent>
+              <ScheduleModalText>Qual o nível da consulta</ScheduleModalText>
+              <BoxInput>
+                <InputScheduleModal_1
+                  onPress={() =>
+                    setAgendamento({
+                      ...agendamento,
+                      prioridadeId: "D083C4DC-66E3-4856-BBFB-159764AA958D",
+                      prioridadeLabel: "Rotina"
+                    })
+                  }
+                >
+                  <Text style={styles.placeHolderNivel}>Rotina</Text>
+                </InputScheduleModal_1>
+                <InputScheduleModal_1
+                  onPress={() =>
+                    setAgendamento({
+                      ...agendamento,
+                      prioridadeId: "07033F9A-4E0E-4F1B-928F-08990DF17761",
+                      prioridadeLabel: "Exame"
+                    })
+                  }
+                >
+                  <Text style={styles.placeHolderNivel}>Exame</Text>
+                </InputScheduleModal_1>
+                <InputScheduleModal_2
+                  onPress={() =>
+                    setAgendamento({
+                      ...agendamento,
+                      prioridadeId: "63261F6A-F19C-4651-A4DB-3601D68677E7",
+                      prioridadeLabel: "Urgencia"
+                    })
+                  }
+                >
+                  <Text style={styles.placeHolderNivel}>Urgência</Text>
+                </InputScheduleModal_2>
+              </BoxInput>
+              <Text style={{ color: "red", marginRight: "46.5%" }}>{erroNivel}</Text>
 
-    return (
-        <ModalMedicalRecord {...rest} visible={visible} transparent={true} animationType="fade">
-            <ScheduleModalView>
-                <BoxModalSchedule>
+              <ScheduleModalText>Informe a localização desejada</ScheduleModalText>
 
-                    <ContainerBoxAlign>
-                        <TitleModal>Agendar consulta</TitleModal>
-                        <BoxContent>
-                            <ScheduleModalText>Qual o nível da consulta</ScheduleModalText>
+              <SelectList
+                boxStyles={{ width: "100%", height: 70, alignItems: "center", marginTop: 20 }}
+                fontFamily="Quicksand_500Medium"
+                searchPlaceholder="Pesquise"
+                placeholder="Selecione uma cidade"
+                maxHeight={100}
+                dropdownTextStyles={{ fontSize: 18 }}
+                inputStyles={{ fontSize: 18 }}
+                setSelected={(txt) =>
+                  setAgendamento({
+                    ...agendamento,
+                    localizacao: txt
+                  })
+                }
+                notFoundText="Nenhum dado encontrado"
+                data={dePara(listaLocalizacao)}
+                save="value"
+              />
 
-                            <BoxInput>
-                                <InputScheduleModal_1
-                                    onPress={() => setAgendamento({
-                                        ...agendamento,
+              <Text style={{ color: "red", marginRight: "46.5%" }}>{erroLocalizacao}</Text>
 
-                                        prioridadeId: 'D083C4DC-66E3-4856-BBFB-159764AA958D',
-                                        prioridadeLabel: 'Rotina'
-                                    })}
-                                ><Text style={styles.placeHolderNivel}  >Rotina</Text></InputScheduleModal_1>
-                                <InputScheduleModal_1
-                                    onPress={() => setAgendamento({
-                                        ...agendamento,
+              <ButtonSchedule onPress={() => HandleContinue()}>
+                <ButtonTitle>continuar</ButtonTitle>
+              </ButtonSchedule>
 
-                                        prioridadeId: '07033F9A-4E0E-4F1B-928F-08990DF17761',
-                                        prioridadeLabel: 'Exame'
-                                    })}
-                                >
-                                    <Text style={styles.placeHolderNivel}  >Exame</Text>
-                                </InputScheduleModal_1>
-                                <InputScheduleModal_2
-                                    onPress={() => setAgendamento({
-                                        ...agendamento,
-
-                                        prioridadeId: '63261F6A-F19C-4651-A4DB-3601D68677E7',
-                                        prioridadeLabel: 'Urgencia'
-                                    })}
-                                    
-                                ><Text style={styles.placeHolderNivel}  >Urgência</Text></InputScheduleModal_2>
-                            </BoxInput>
-
-                            <Text style={{ color: 'red', marginRight: "46.5%" }}>{erroNivel}</Text>
-
-
-                            <ScheduleModalText>Informe a localização desejada</ScheduleModalText>
-                            
-                     
-                           
-                            <SelectList
-
-                                data={listaLocalizacao}
-                                placeholder="Informe a localização"
-                                value={agendamento ? agendamento.localizacao : null}
-                                onSelect={(item)=> setAgendamento({
-                                    ...agendamento,
-                                    localizacao:item
-                                })}
-                            
-                            />
-                     
-
-                         
-                            {/* <InputScheduleModal_3
-                                placeholder="Informe a localização"
-                                value={agendamento ? agendamento.localizacao : null}
-                                onChangeText={(txt) => setAgendamento({
-                                    ...agendamento, //Mantendo as informacoes de agendamento
-                                    localizacao: txt
-                                })}
-                            /> */}
-
-                            <Text style={{ color: 'red', marginRight: "46.5%" }}>{erroLocalizacao}</Text>
-
-
-
-                            <ButtonSchedule onPress={() => HandleContinue()}>
-                                <ButtonTitle>continuar</ButtonTitle>
-                            </ButtonSchedule>
-
-                            <ButtonCancel onPress={() => Cancelar()}>
-                                <LinkCancel>Cancelar</LinkCancel>
-                            </ButtonCancel>
-
-                        </BoxContent>
-                    </ContainerBoxAlign>
-
-                </BoxModalSchedule>
-            </ScheduleModalView>
-        </ModalMedicalRecord>
-    )
-}
-
+              <ButtonCancel onPress={() => Cancelar()}>
+                <LinkCancel>Cancelar</LinkCancel>
+              </ButtonCancel>
+            </BoxContent>
+          </ContainerBoxAlign>
+        </BoxModalSchedule>
+      </ScheduleModalView>
+    </ModalMedicalRecord>
+  );
+};
 
 const styles = StyleSheet.create({
-    placeHolderNivel:{
-        color:"#34898f"
-    }
-})
+  placeHolderNivel: {
+    color: "#34898f"
+  }
+});
